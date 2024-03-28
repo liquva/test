07da1705139ed148e0ddeb9dadd7f938bc23879b
liquva <hotyanovich86@gmail.com>
2024-03-28 08:27:23 +0300

    commit3

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,6 @@ public class Main {
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
             System.out.println("i = " + i);
         }
     }
