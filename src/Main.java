7cb7bdddc414cc9d14c69c64c7001b54e939db16
liquva <hotyanovich86@gmail.com>
2024-03-28 08:27:46 +0300

    commit5

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
